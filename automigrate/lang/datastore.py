565e42096238147fc77728e824e03aed9c75631a
Bryan Tan <techniux@gmail.com>
2019-11-05 18:29:10 -0800

    Update datastore language, add synthesis code

--- automigrate/lang/datastore.py
+++ automigrate/lang/datastore.py
@@ -25,19 +25,40 @@ def build_datastore_v1_spec(input_types: List[str], output_type: str):
     type_ns = types.SimpleNamespace(**{
         "Key": spec.type.EnumType("Key", keys),
         "Value": spec.type.EnumType("Value", data),
-        "EmptyState": spec.type.EnumType("EmptyState", ["empty"]),
+        "InitState": spec.type.EnumType("InitState", ["init"]),
         "String": spec.type.ValueType("String"),
         "Status": spec.type.ValueType("Status"),
-        "State": spec.type.ValueType("State", [("status", spec.expr.ExprType.INT)]),
+        "State": spec.type.ValueType("State", [("status", spec.expr.ExprType.INT), ("size", spec.expr.ExprType.INT)]),
+        # See source file for tyrell.enumerator.smt.SmtEnumerator
+        # Note that NO type can contain "Empty" or else SMT enumerator fails
+        "Empty": spec.type.ValueType("Empty"),
     })
     for t in type_ns.__dict__.values():
         tys.define_type(t)
 
+    import tyrell.spec.expr as E
+    BOp = E.BinaryOperator
+
     prods = spec.spec.ProductionSpec()
-    prods.add_func_production(name="const_empty", lhs=type_ns.State, rhs=[type_ns.EmptyState])
+    prods.add_func_production(name="const_init", lhs=type_ns.State, rhs=[type_ns.InitState])
     prods.add_func_production(name="const_key", lhs=type_ns.String, rhs=[type_ns.Key])
     prods.add_func_production(name="const_val", lhs=type_ns.String, rhs=[type_ns.Value])
-    prods.add_func_production(name="store_file", lhs=type_ns.State, rhs=[type_ns.String, type_ns.String, type_ns.State])
+    prods.add_func_production(
+        name="store_file",
+        lhs=type_ns.State, rhs=[type_ns.String, type_ns.String, type_ns.State],
+        constraints=[
+            E.BinaryExpr(
+                BOp.EQ,
+                E.PropertyExpr("size", E.ExprType.INT, E.ParamExpr(0)),
+                E.BinaryExpr(
+                    BOp.ADD,
+                    E.PropertyExpr("size", E.ExprType.INT, E.ParamExpr(3)),
+                    E.ConstExpr(1)
+                )
+            )
+        ]
+    )
+    prods.add_func_production(name="empty", lhs=type_ns.Empty, rhs=[type_ns.Empty])
 
     prog_spec = spec.spec.ProgramSpec("program", [getattr(type_ns, n) for n in input_types],
                                       getattr(type_ns, output_type))
@@ -51,7 +72,7 @@ class DatastoreV1Interpreter(PostOrderInterpreter):
     def eval_Value(self, val):
         return val
 
-    def eval_EmptyState(self, val):
+    def eval_InitState(self, val):
         return State(status=StoreStatus.SUCCESS, store={})
 
     def eval_StatusCode(self, val):
@@ -66,8 +87,14 @@ class DatastoreV1Interpreter(PostOrderInterpreter):
     def eval_const_status(self, node, args):
         return args[0]
 
-    def eval_const_empty(self, node, args):
+    def eval_const_init(self, node, args):
         return args[0]
 
     def eval_store_file(self, node, args):
-        return State(status=StoreStatus.SUCCESS, store={args[0]: args[1], **args[2].store})
+        return State(status=StoreStatus.SUCCESS, store={**args[2].store, args[0]: args[1]})
+
+    def apply_status(self, state):
+        return int(state.status)
+
+    def apply_size(self, state):
+        return len(state.store)
--- main.py
+++ main.py
@@ -8,19 +8,41 @@ import tyrell
 
 def parse_args(args: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("synth_file", help="Synthesis configuration file")
+    parser.add_argument("synth_file", help="Synthesis configuration file",
+                        type=argparse.FileType("r", encoding="utf8"))
+    parser.add_argument("-d", "--depth", help="Enumeration depth", type=int, default=5)
     ns = parser.parse_args(args)
     return ns
 
 
 def main(args_: List[str]):
     args = parse_args(args_)
-    print("TODO: Parse stuff {}".format(args.synth_file))
     interpreter = DS.DatastoreV1Interpreter()
-    builder = tyrell.dsl.Builder(DS.build_datastore_v1_spec(["String", "String"], "State"))
-    progn = """(store_file (@param 0) (const_val (Value "data1")) (store_file (const_key (Key "file3")) (const_val (Value "data1")) (const_empty (EmptyState "empty"))))"""
-    result = interpreter.eval(builder.from_sexp_string(progn), ["foo", "bar"])
-    print(result)
+    spec = DS.build_datastore_v1_spec(["String", "String"], "State")
+    builder = tyrell.dsl.Builder(spec)
+    with args.synth_file as f:
+        progn = "\n".join(f.readlines())
+
+    # Sample code for now. This just evaluates the given program and then uses
+    # the input/output pair to synthesize an equivalent program.
+    inputs = ["foo", "bar"]
+    outputs = interpreter.eval(builder.from_sexp_string(progn), inputs)
+    print("Evaluation result:", outputs)
+
+    # enumerator = tyrell.enumerator.ExhaustiveEnumerator(spec, max_depth=args.depth)
+    enumerator = tyrell.enumerator.SmtEnumerator(spec, depth=args.depth, loc=3)
+    decider = tyrell.decider.ExampleConstraintPruningDecider(
+        spec=spec,
+        interpreter=interpreter,
+        examples=[
+            tyrell.decider.Example(input=inputs, output=outputs)
+        ]
+    )
+    synthesizer = tyrell.synthesizer.Synthesizer(enumerator=enumerator, decider=decider)
+    prog_syn = synthesizer.synthesize()
+    print("Synthesized program:", prog_syn)
+    if prog_syn:
+        print("Synthesized result:", interpreter.eval(prog_syn, inputs))
 
 
 if __name__ == "__main__":
