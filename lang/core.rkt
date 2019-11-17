9df9d8b5680a77b3a95e13e2e48b5d097e6581c5
Bryan Tan <techniux@gmail.com>
2019-11-17 01:33:56 -0800

    Add support for language-specific context constructs in core

--- lang/core.rkt
+++ lang/core.rkt
@@ -31,6 +31,7 @@
       (ife Ee (expr ...) (expr ...))
       (let x Ee))
   (vars ((x v) ...)) ;; variables
+  (ctx (void)) ;; language-specific context
   (v number string boolean (void)))
 
 (define-metafunction
@@ -49,73 +50,77 @@
 (define red
   (reduction-relation
    CoreE
-   #:domain (expr vars)
+   #:domain (expr ctx vars)
    ; Variable loads
-   (--> [(in-hole Ee x) vars]
-        [(in-hole Ee (load-var vars x)) vars]
+   (--> [(in-hole Ee x) ctx vars]
+        [(in-hole Ee (load-var vars x)) ctx vars]
         "var")
    ; Binary operations
-   (--> [(in-hole Ee (+ number ...)) vars]
-        [(in-hole Ee ,(apply + (term (number ...)))) vars]
+   (--> [(in-hole Ee (+ number ...)) ctx vars]
+        [(in-hole Ee ,(apply + (term (number ...)))) ctx vars]
         "+")
-   (--> [(in-hole Ee (or boolean ...)) vars]
-        [(in-hole Ee ,(ormap identity (term (boolean ...)))) vars]
+   (--> [(in-hole Ee (or boolean ...)) ctx vars]
+        [(in-hole Ee ,(ormap identity (term (boolean ...)))) ctx vars]
         "or")
-   (--> [(in-hole Ee (and boolean ...)) vars]
-        [(in-hole Ee ,(andmap identity (term (boolean ...)))) vars]
+   (--> [(in-hole Ee (and boolean ...)) ctx vars]
+        [(in-hole Ee ,(andmap identity (term (boolean ...)))) ctx vars]
         "and")
-   (--> [(in-hole Ee (eq? v_1 v_2)) vars]
-        [(in-hole Ee ,(apply eq? (term (v_1 v_2)))) vars]
+   (--> [(in-hole Ee (eq? v_1 v_2)) ctx vars]
+        [(in-hole Ee ,(apply eq? (term (v_1 v_2)))) ctx vars]
         "eq")
+   ; Let
+   (--> [(in-hole Ee (let x v)) ctx vars]
+        [(in-hole Ee v) ctx (extend-vars vars x v)]
+        "let")
    ; If expressions
-   (--> [(in-hole Ee (ife #t (expr_t ...) (expr_f ...))) vars]
-        [(in-hole Ee (block expr_t ...)) vars]
+   (--> [(in-hole Ee (ife #t (expr_t ...) (expr_f ...))) ctx vars]
+        [(in-hole Ee (block expr_t ...)) ctx vars]
         "if_t")
-   (--> [(in-hole Ee (ife #f (expr_t ...) (expr_f ...))) vars]
-        [(in-hole Ee (block expr_f ...)) vars]
+   (--> [(in-hole Ee (ife #f (expr_t ...) (expr_f ...))) ctx vars]
+        [(in-hole Ee (block expr_f ...)) ctx vars]
         "if_f")
    ; Blocks
-   (--> [(in-hole Ee (block v)) vars]
-        [(in-hole Ee v) vars]
+   (--> [(in-hole Ee (block v)) ctx vars]
+        [(in-hole Ee v) ctx vars]
         "block_v")
-   (--> [(in-hole Ee (block v expr expr_n ...)) vars]
-        [(in-hole Ee (block expr expr_n ...)) vars]
+   (--> [(in-hole Ee (block v expr expr_n ...)) ctx vars]
+        [(in-hole Ee (block expr expr_n ...)) ctx vars]
         "block_n")
    ))
 
 (define-metafunction
   CoreE
-  eval-expr : expr vars -> v or stuck
-  [(eval-expr v vars) v]
-  [(eval-expr expr vars)
-   (eval-expr expr_next vars_next)
-   (where ((expr_next vars_next)) ,(apply-reduction-relation red (term (expr vars))))]
-  [(eval-expr any_1 any_2) stuck])
+  eval-expr : expr ctx vars -> v or stuck
+  [(eval-expr v ctx vars) v]
+  [(eval-expr expr ctx vars)
+   (eval-expr expr_next ctx_next vars_next)
+   (where ((expr_next ctx_next vars_next)) ,(apply-reduction-relation red (term (expr ctx vars))))]
+  [(eval-expr any_1 any_2 any_3) stuck])
 
 (module+ test
   (define my-vars (term ((x 10) (y 20) (z 30))))
   (test-->> red
-            (term (x ,my-vars))
-            (term (10 ,my-vars)))
+            (term (x (void) ,my-vars))
+            (term (10 (void) ,my-vars)))
   (test-->> red
-            (term ((+ (+ 30 40) (+ 10 20)) ()))
-            (term (100 ())))
+            (term ((+ (+ 30 40) (+ 10 20)) (void) ()))
+            (term (100 (void) ())))
   (test-->> red
-            (term ((or #f #f #t) ()))
-            (term (#t ())))
+            (term ((or #f #f #t) (void) ()))
+            (term (#t (void) ())))
   (test-->> red
-            (term ((and #f #f #t) ()))
-            (term (#f ())))
+            (term ((and #f #f #t) (void) ()))
+            (term (#f (void) ())))
   (test-->> red
-            (term ((eq? 1 1) ()))
-            (term (#t ())))
+            (term ((eq? 1 1) (void) ()))
+            (term (#t (void) ())))
   (test-->> red
-            (term ((ife #t (1) (0)) ()))
-            (term (1 ())))
+            (term ((ife #t (1) (0)) (void) ()))
+            (term (1 (void) ())))
   (test-->> red
-            (term ((ife #f (1) (0)) ()))
-            (term (0 ())))
+            (term ((ife #f (1) (0)) (void) ()))
+            (term (0 (void) ())))
   (test-->> red
-            (term ((ife (eq? x y) (x) ((+ x y))) ,my-vars))
-            (term (30 ,my-vars)))
-  (test-equal (term (eval-expr x ,my-vars)) 10))
+            (term ((ife (eq? x y) (x) ((+ x y))) (void) ,my-vars))
+            (term (30 (void) ,my-vars)))
+  (test-equal (term (eval-expr x (void) ,my-vars)) 10))
