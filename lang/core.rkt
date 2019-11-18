d08237c3c38c4d8333548d37f3e94a1430d9b910
Bryan Tan <techniux@gmail.com>
2019-11-17 17:05:13 -0800

    Use explicit fixed-point style for interpreter for extensibility

--- lang/core.rkt
+++ lang/core.rkt
@@ -1,6 +1,6 @@
 #lang rosette
 
-(provide binop? get-op eval-expr run-program)
+(provide binop? get-op core-eval core-eval/fold run-program)
 (require racket/function)
 
 (define (ops/or . vals) (ormap identity vals))
@@ -20,44 +20,50 @@
         [`(,_ ,f) (eval f ns)]))))
 (define (binop? op) (not (eq? (get-op op) #f)))
 
-(define (eval-expr/fold exprs ctx vars)
+(define (core-eval/fold f exprs ctx vars)
   (for/fold ([vals '()] [c ctx] [v vars])
             ([e exprs])
-    (match-let ([`(,valn ,cn ,vn) (eval-expr e c v)])
+    (match-let ([`(,valn ,cn ,vn) (f e c v)])
       (values (cons valn vals) cn vn))))
 
-(define/match (eval-expr expr ctx vars)
-  [((? number? lit) _ _) `(,lit ,ctx ,vars)]
-  [((? boolean? lit) _ _) `(,lit ,ctx ,vars)]
-  [((? symbol? x) _ _)
-   `(,(cadr (assoc x vars)) ,ctx ,vars)]
-  [(`(let ,(? symbol? x) ,expr) _ _)
-   (match-let ([(list val nctx nvars) (eval-expr expr ctx vars)])
-     `(,val ,nctx ((x ,val) ,@nvars)))]
-  [(`(,(? binop? op) ,e ...) _ _)
-   (let-values
-       ([(vals nctx nvars) (eval-expr/fold e ctx vars)]
-        [(opf) (get-op op)])
-     `(,(apply opf vals) ,nctx ,nvars))]
-  [(`(block) _ _) `((void) ,ctx ,vars)]
-  [(`(block ,e) _ _) (eval-expr e ctx vars)]
-  [(`(block ,e ,en ...) _ _)
-   (match-let ([(list val nctx nvars) (eval-expr e ctx vars)])
-     (eval-expr `(block ,@en) nctx nvars))]
-  [(`(if ,guard ,texpr ,fexpr) _ _)
-   (match-let ([(list guardv nctx nvars) (eval-expr guard ctx vars)])
-     (eval-expr (if guardv texpr fexpr) nctx nvars))]
-  [(_ _ _) 'stuck])
+; Fixed point evaluation.
+; This is extensible
+(define (core-eval f expr ctx vars)
+  (match expr
+    [(? number? lit) `(,lit ,ctx ,vars)]
+    [(? boolean? lit) `(,lit ,ctx ,vars)]
+    [(? symbol? x)
+     `(,(cadr (assoc x vars)) ,ctx ,vars)]
+    [`(let ,(? symbol? x) ,expr)
+     (match-let ([(list val nctx nvars) (f expr ctx vars)])
+       `(,val ,nctx ((x ,val) ,@nvars)))]
+    [`(,(? binop? op) ,e ...)
+     (let-values
+         ([(vals nctx nvars) (core-eval/fold f e ctx vars)]
+          [(opf) (get-op op)])
+       `(,(apply opf vals) ,nctx ,nvars))]
+    [`(block) `((void) ,ctx ,vars)]
+    [`(block ,e) (f e ctx vars)]
+    [`(block ,e ,en ...)
+     (match-let ([(list val nctx nvars) (f e ctx vars)])
+       (f `(block ,@en) nctx nvars))]
+    [`(if ,guard ,texpr ,fexpr)
+     (match-let ([(list guardv nctx nvars) (f guard ctx vars)])
+       (f (if guardv texpr fexpr) nctx nvars))]
+    [_ 'stuck]))
 
-(define (run-program prog)
-  (match-let ([(list val ctx vars) (eval-expr prog '() '())])
+(define (run-program interpreter prog)
+  (define (eval-fun expr ctx vars)
+    (interpreter eval-fun expr ctx vars))
+  (match-let ([(list val ctx vars) (eval-fun prog '() '())])
     val))
 
 (module+ test
   (require rackunit)
-  (check-equal? (run-program 10) 10)
-  (check-equal? (run-program '(+ 10 20 30)) 60)
-  (check-equal? (run-program '(block 10 20)) 20)
-  (check-equal? (run-program '(if (|| #t #f) 10 20)) 10)
-  (check-equal? (run-program '(if (|| #f #f) 10 20)) 20)
+  (define (run prog) (run-program core-eval prog))
+  (check-equal? (run 10) 10)
+  (check-equal? (run '(+ 10 20 30)) 60)
+  (check-equal? (run '(block 10 20)) 20)
+  (check-equal? (run '(if (|| #t #f) 10 20)) 10)
+  (check-equal? (run '(if (|| #f #f) 10 20)) 20)
   )
