1321c206d62833f6e8751633a699efe9e9dfd6ad
Bryan Tan <techniux@gmail.com>
2019-11-29 18:51:49 -0800

    Update translators to translate nested subexpressions

--- compiler.rkt
+++ compiler.rkt
@@ -7,7 +7,7 @@
 (require "lang/datastore.rkt")
 
 ; Recursive descent into subexpressions
-(define (translate-core f prog)
+(define (translate-core cont f prog)
   (match prog
     [`(let ,(? symbol? x) ,expr) `(let ,x ,(f expr))]
     [`(,(? binop? op) ,e ...) `(,op ,(f e) ...)]
@@ -16,23 +16,23 @@
     [_ prog]))
 
 ; Translate datastore v1 into datastore v2
-(define (translate-ds-v1-v2 f prog)
+(define (translate-ds-v1-v2 cont f prog)
   (match prog
-    [`(store ,key ,val) `(block (if (contains ,key) (del ,key) (void)) (store ,key ,val))]
-    [`(del ,key) `(if (contains ,key) (del ,key) (void))]
-    [`(get ,key) `(if (contains ,key) (get ,key) #f)]
-    [_ (f prog)]
+    [`(store ,key ,val) `(block (if (contains ,(f key)) (del ,(f key)) (void)) (store ,(f key) ,(f val)))]
+    [`(del ,key) `(if (contains ,(f key)) (del ,(f key)) (void))]
+    [`(get ,key) `(if (contains ,(f key)) (get ,(f key)) #f)]
+    [_ (cont f prog)]
     ))
 
 ; Compose translators from right to left (i.e. t2 is executed before t1)
 (define (compose-translate t1 t2)
-  (lambda (f prog)
-    (define (ct p) (t2 f prog))
-    (t1 ct prog)))
+  (lambda (cont f prog)
+    (define (ct f p) (t2 cont f prog))
+    (t1 ct f prog)))
 
 (define (run-translate translator prog)
   (define (trans prog)
-    (translator trans prog))
+    (translator identity trans prog))
   (trans prog))
 
 (module+ test
@@ -46,4 +46,11 @@
   (check-equal? (v1v2trans '(block (store x 10) (get x)))
                 '(block (block (if (contains x) (del x) (void)) (store x 10))
                         (if (contains x) (get x) #f)))
+  (check-equal? (v1v2trans '(block (store 1 2) (store 2 3) (del (get 1))))
+                '(block
+                  (block (if (contains 1) (del 1) (void)) (store 1 2))
+                  (block (if (contains 2) (del 2) (void)) (store 2 3))
+                  (if (contains (if (contains 1) (get 1) #f))
+                      (del (if (contains 1) (get 1) #f))
+                      (void))))
   )
